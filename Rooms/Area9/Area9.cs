6a2ba0596f13f2760d45697476f58380e87d86ae
ToboterXP <55210408+ToboterXP@users.noreply.github.com>
2022-07-04 13:23:07 +0200

    Sketched out new Area

---
+++ Rooms/Area9/Area9.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKSecondQuest.Rooms.Area9
+{
+    class Area9 : Room
+    {
+        public Area9() : base("Area 9") { }
+    }
+}
---
+++ Rooms/Area9/Waterways1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKSecondQuest.Rooms.Area9
+{
+    class Waterways1
+    {
+    }
+}
